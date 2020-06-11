85e9c83e8f56842692f2040bf8030b30710ff57b
Jason Shaner <jshaner@cnm.edu>
2020-06-11 14:57:43 -0600

    Added throwables.

--- app/src/main/java/edu/cnm/deepdive/animals/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/animals/viewmodel/MainViewModel.java
@@ -12,6 +12,7 @@ import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.service.AnimalService;
+import io.reactivex.schedulers.Schedulers;
 import java.io.IOException;
 import java.util.List;
 import retrofit2.Response;
@@ -21,83 +22,31 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainViewModel extends AndroidViewModel {
 
   private MutableLiveData<List<Animal>> animals;
+  private MutableLiveData<Throwable> throwable;
+  private AnimalService animalService;
 
   public MainViewModel(@NonNull Application application) {
     super(application);
+    animalService = AnimalService.getInstance();
     animals = new MutableLiveData<>();
+    throwable = new MutableLiveData<>();
     loadAnimals();
   }
 
   public LiveData<List<Animal>> getAnimals() {
     return animals;
+  }
 
+  public LiveData<Throwable> getThrowable() {
+    return throwable;
   }
 
   private void loadAnimals() {
 
-    new AsyncTask<Void, Void, List<Animal>>() {
-
-      AnimalService animalService;
-
-      @Override
-      protected void onPreExecute() {
-        super.onPreExecute();
-        Gson gson = new GsonBuilder()
-            .excludeFieldsWithoutExposeAnnotation()
-            .create();
-        Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build();
-        Log.d("AnimalService", "before service");
-
-        animalService = retrofit.create(AnimalService.class);
-
-      }
-
-      @Override
-      protected List<Animal> doInBackground(Void... voids) {
-
-
-        try {
-          Log.d("AnimalService", "before request");
-
-          Response<List<Animal>> response = animalService.getAnimals(BuildConfig.CLIENT_KEY)
-              .execute();
-          Log.d("AnimalService", "after request");
-
-          if (response.isSuccessful()) {
-            Log.d("AnimalService", "response is successful");
-
-            List<Animal> animals = response.body();
-            Log.d("AnimalService", String.valueOf(animals));
-
-            assert animals != null;
-            MainViewModel.this.animals.postValue(animals);
-            return animals;
-
-
-          } else {
-            cancel(true);
-            Log.d("AnimalService", "response is unsuccessful");
-
-            Log.e("AnimalService", response.message());
-          }
-
-        } catch (IOException e) {
-          Log.e("AnimalService", e.getMessage(), e);
-          cancel(true);
-        }
-
-        return null;
-      }
-
-
-
-
-
-
-
-    }.execute();
+    animalService.getAnimals(BuildConfig.CLIENT_KEY)
+        .subscribeOn(Schedulers.io())
+        .subscribe(animals -> this.animals.postValue(animals),
+            (throwable) -> this.throwable.postValue(throwable));
   }
+
 }
